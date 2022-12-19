bc1cdc54643f5ace67e11cce04f210c08540cde7
Wea-boo <makotonaegi125@gmail.com>
2022-12-19 10:17:08 +0100

    finished checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    Fullname: "Walter H. White",
+    bio: "Nothing much to talk about, just watch Breaking Bad lol",
+    imgSrc: "/Walter_White_S5B.png",
+    profession: "Chemistry Nobel Prize winner, highschool chemistry teacher and Meth Kingpin",
+    shows: false,
+    seconds: 0,
+ 
+  }
+
+  componentDidMount = () => {
+    this.interval = setInterval(() => {
+      this.setState(previous => ({
+         seconds: previous.seconds+1
+      }))
+
+    }, 1000)
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval)
+  }
+
+  handleClick = () => { this.setState({shows:!this.state.shows}) }
+  render() {
+    return (
+      <div>
+        <p>the last component was rendered {this.state.seconds} seconds ago</p>
+        <button onClick={this.handleClick}>Show Profile</button>
+        {this.state.shows ? 
+          <div>           
+            <img src={this.state.imgSrc} alt="" />
+            <h1>{this.state.Fullname}</h1>
+            <h4>{this.state.bio}</h4>
+            <p>{this.state.profession}</p>
+
+          </div>
+          : ''
+        }
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
